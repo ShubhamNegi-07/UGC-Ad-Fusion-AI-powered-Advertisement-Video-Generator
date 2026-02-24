349f8b1b5e0628778b40fa80c27e68b6cc167a2e
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-24 23:27:17 +0530

    update projectRoutes file

--- server/routes/projectRoutes.ts
+++ server/routes/projectRoutes.ts
@@ -4,9 +4,9 @@ getAllPublishedProjects } from '../controllers/projectController.js';
 import { protect } from '../middlewares/auth.js';
 import upload from '../configs/multer.js';
 
-const projectRouter = express. Router()
+const projectRouter = express.Router()
 
-projectRouter.post('/create', upload.array('images, 2'), protect, createProject)
+projectRouter.post('/create', upload.array('images', 2), protect, createProject)
 projectRouter.post('/video', protect, createVideo)
 projectRouter.get('/published', getAllPublishedProjects)
 projectRouter.delete('/:projectId', protect, deleteProject)
