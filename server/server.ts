243eafbb800543664e90952bea877d7bc7fe951b
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-03-04 21:22:25 +0530

    feat: add basic request logging middleware

--- server/server.ts
+++ server/server.ts
@@ -15,10 +15,10 @@ const PORT = process.env.PORT || 5000;
 //Middleware
 app.use(cors())
 
-app.use((req, res, next) => 
+app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
-})
+}
 
 app.post('/api/clerk', express.raw({ type: 'application/json'}), clerkWebhooks)
 
