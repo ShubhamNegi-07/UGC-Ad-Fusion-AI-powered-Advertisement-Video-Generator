1220b26be856d11c1ca175963e5335c94b7b9494
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-03-04 21:22:08 +0530

    feat: add basic request logging middleware

--- server/server.ts
+++ server/server.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 //Middleware
 app.use(cors())
 
-app.use((req, res, next) => {
+app.use((req, res, next) => 
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
 })
