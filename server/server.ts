bb89d10a7380b45a04db0e27cec137371eb25f2d
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-03-07 21:38:29 +0530

    feat: add basic request logging middleware

--- server/server.ts
+++ server/server.ts
@@ -27,9 +27,9 @@ app.use(clerkMiddleware())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
-;
+});
 
-app.get("/debug-sentry", function mainHandler(req, res) {
+app.get("/debug-sentry", function mainHandler(req, res {
   throw new Error("My first Sentry error!");
 });
 
