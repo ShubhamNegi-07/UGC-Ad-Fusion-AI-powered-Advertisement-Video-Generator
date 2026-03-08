48d6f9c57a4dd7a88e0ad0958f7d588416799037
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-03-08 21:06:50 +0530

    fix: refactor Cloudinary config and update Gemini model integration

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -8,7 +8,10 @@ import path from 'path';
 import ai from '../configs/ai.js';
 import axios from 'axios';
 
-cloudit: process.env.CLOUDINARY_API_SECRET,
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
 });
 
