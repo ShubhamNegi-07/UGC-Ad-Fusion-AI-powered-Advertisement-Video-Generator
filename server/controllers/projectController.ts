13e41c42bfb1c20049b804dae58aca0a2cbdfa23
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-03-19 20:46:31 +0530

    fix: refactor Cloudinary config and update Gemini model integration

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -78,121 +78,13 @@ export const createProject = async (req:Request, res: Response) => {
         const model = "gemini-1.5-flash"
         const generationConfig: GenerateContentConfig = {
             maxOutputTokens: 32768,
-            temperature
-                {
-                    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-                    threshold: HarmBlockThreshold.OFF,
-                },
-                 {
-                    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-                    threshold: HarmBlockThreshold.OFF,
-                },
-                 {
-                    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-                    threshold: HarmBlockThreshold.OFF,
-                },
-            ]    
-        } 
-        
-        // image to base64 structure for ai model
-        const img1base64 = loadImage(images[0].path, images[0].mimetype);
-        const img2base64 = loadImage(images[1].path, images[1].mimetype);
-        const prompt = {
-            text: `Combine the person and product into a realistic photo.
-            Make the person naturally hold or use the product.
-            Match lighting, shadows, scale and perspective.
-            Make the person stand in professional studio lighting.
-            Output e-commerce-quality photo realistic imagery.
-            ${userPrompt}`
-        }
-
-        // Generate the image using the ai model
-        const response: any = await ai.models.generateContent({
-            model,
-             contents: [
-                {
-                    role: 'user',
-                    parts: [img1base64, img2base64, prompt]
-                }
-            ],
-            config: generationConfig,
-        })
-
-        // Check if the response is valid
-        if(!response?.candidates?.[0]?.content?.parts) {
-            throw new Error('Unexpected response')
-        }
-
-        const parts = response.candidates[0].content.parts;
-
-        let finalBuffer: Buffer | null = null
-
-        for(const part of parts){
-            if(part.inlineData){
-                finalBuffer = Buffer.from(part.inlineData.data, 'base64')
-            }
-        }
-
-        if(!finalBuffer){
-            throw new Error('Failed to generate image');
-        }
-
-        const base64Image = `data:image/png;base64,${finalBuffer.toString
-            ('base64')}`
-
-        const uploadResult = await cloudinary.uploader.upload(base64Image,
-            {resource_type: 'image'});
-
-            await prisma.project.update({
-                where: {id: project.id},
-                data: {
-                    generatedImage: uploadResult.secure_url,
-                    isGenerating: false
-                }
-            })
-
-            res.json({projectId: project.id})
-
-    } catch (error:any) {
-        if(tempProjectId!){
-            // update project status and error message
-            await prisma.project.update({
-                where: {id: tempProjectId},
-                data: {isGenerating: false, error: error.message}
-            })
-        }
-        if(isCreditDeducted){
-            // add credits back
-            await prisma.user.update({
-                where: {id: userId},
-                data: {credits: {increment: 5}}
-            })
-        }
-        console.error('[createProject Error]', error?.message || error);
-        Sentry.captureException(error);
-        res.status(500).json({ message: error.message });
-    }
-}
-
-export const createVideo = async (req:Request, res: Response) => {
-    const {userId} = req.auth()
-    const { projectId } = req.body;
-    let isCreditDeducted = false;
-
-    const user = await prisma.user.findUnique({
-        where: {id: userId}
-    })
-    if(!user || user.credits < 10){
-        return res.status(401).json({ message: 'Insufficient credits' });
-    }
-
-    //deduct credits for video generation
-    await prisma.user.update({
-        where: {id: userId},
-        data: {credits: {decrement: 10}}
-    }).then(()=>{ isCreditDeducted = true} );
-
-    try {
+            temperature: 1,
+            topP: 0.95,
+            responseModalities: ['IMAGE'],
+            imageConfig: {
+                aspectRatio: aspectRatio || '9:16',
+                imageSize: '1K'
+           
 
         const project = await prisma.project.findUnique({
             where: {id: projectId, userId},
