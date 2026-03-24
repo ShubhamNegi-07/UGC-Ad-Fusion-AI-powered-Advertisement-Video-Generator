bb5dad636b299e51b3bfeda86cf4de6c909e986c
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-03-24 21:13:56 +0530

    feat: update Generator page logic and UI

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -107,18 +107,18 @@ const Generator = () => {
             </div>
             <div className="mb-4 text-gray-300">
               <label htmlFor="productName" className="block text-sm mb-4">
-                Product Name
-              </label>
-              <input
-                type="text"
-                id="productName"
+               "productName"
                 value={productName}
                 onChange={(e) => setProductName(e.target.value)}
                 placeholder="Enter the name of the project"
                 required
                 className="w-full bg-white/3 rounede-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 
                 outline-none transition-all"
-             "productDescription"
+              />
+            </div>
+            <div className="mb-4 text-gray-300">
+              <label
+                htmlFor="productDescription"
                 className="block text-sm mb-4"
               >
                 Product Description{" "}
